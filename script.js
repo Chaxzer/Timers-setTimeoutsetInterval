beb09c23e24c32e7a16bacc733c00a0baf8c1c2e
Chaxzer <110409740+Chaxzer@users.noreply.github.com>
2024-01-19 01:56:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// alert("Bola Mahadev");
+
+
+  // shambhu = () =>{
+  //   setTimeout(
+  //   time.insertAdjacentHTML("afterbegin","<div class='secret' ><h1>mahadeva</h1></div>"),2000)
+  }
+// the function will create a div indide div with id time after 2 seconds
+
+// for (i = 0; i < 5; i++) {
+//   setTimeout(function() {
+//     time.insertAdjacentHTML("afterbegin", "<div class='secret' ><h1>mahadeva</h1></div>");
+//   }, 2000);
+// }
+
+//this will create 5 divs inside div with id time after 2 seconds
+
+//________________timer id_______________________
+
+// let tId = setTimeout(function() {
+//   time.insertAdjacentHTML("afterbegin", "<div class='secret' ><h1>mahadeva</h1></div>");}, 2000);
+
+
+// clearTimeout(tId);      
+        // stopping the execution of tId
+
+//   console.log(tId);
+
+
+//____________________input to stop timer_________________________
+
+// let tlp = setTimeout(function() {
+//   document.getElementById("time").insertAdjacentHTML("afterbegin", "<div class='secret'><h1>jai Mata Rani</h1></div>");
+// }, 2000);
+// let ask = prompt("mata rani aai? yes/no");
+// if (ask === "no") {
+//   clearTimeout(tlp);
+// } 
+
+// else if (ask === "yes") {
+//   document.write("tere pass maa ka ashirvad hai");
+// } 
+
+
+//____________________setTimeout_________________________
+
